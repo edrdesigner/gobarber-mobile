f4310f21f0555b7c48fa3a54bad505285a5bd02e
Eduardo <edrdesigner@gmail.com>
2019-12-01 20:51:08 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import Routes from './routes';
+// import { Container } from './styles';
+
+export default function src() {
+  return <Routes />;
+}
---
+++ src/pages/SignIn/index.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Image } from 'react-native';
+import Background from '~/components/Background';
+
+import logo from '~/assets/logo.png';
+
+import {
+  Container,
+  Form,
+  FormInput,
+  SubmitButton,
+  SignLink,
+  SignLinkText,
+} from './styles';
+
+export default function SignIn() {
+  return (
+    <Background>
+      <Container>
+        <Image source={logo} />
+        <Form>
+          <FormInput
+            icon="mail-outline"
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeHolder="Digite seu e-mail"
+          />
+          <FormInput
+            icon="lock-outline"
+            placeHolder="Sua senha secreta"
+            secureTextEntry
+          />
+
+          <SubmitButton onPress={() => {}}>Acessar</SubmitButton>
+        </Form>
+        <SignLink onPress={() => {}}>
+          <SignLinkText>Criar conta gratuita</SignLinkText>
+        </SignLink>
+      </Container>
+    </Background>
+  );
+}
